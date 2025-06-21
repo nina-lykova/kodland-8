94b72605674bcdd8cb10e2fb8f352631e55d90ac
nina-lykova <lykovanina38@gmail.com>
2025-06-21 13:38:07 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,10 +1,12 @@
 from random import randint
 import requests
+from datetime import datetime, timedelta
+
 
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer, hp_range=(100, 1000), power_range=(100, 1000)):
+    def __init__(self, pokemon_trainer, feed_interval, hp_range=(100, 1000), power_range=(100, 1000)):
 
         self.pokemon_trainer = pokemon_trainer   
 
@@ -17,6 +19,7 @@ class Pokemon:
 
         self.hp = randint(*self.hp_range)
         self.power = randint(*self.power_range)
+        self.feed_interval = feed_interval
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -25,9 +28,8 @@ class Pokemon:
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
-            data = response.json()
-            img_url =  (data["sprites"]["other"]["dream_world"]["front_default"])
-            return print(f"URL картинки: {img_url}")
+            data = response.json()  
+            return (data["sprites"]["other"]["dream_world"]["front_default"])    
         else:
             return "https://i.pinimg.com/originals/b9/c7/c5/b9c7c50da1fc0b8c97b9c75eee2603a2.png"
         
@@ -64,9 +66,26 @@ class Pokemon:
         else:
             enemy.hp = 0
             return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+    
+    #Метод кормления
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"
+
+
 
 class Wizard(Pokemon):
-    pass
+    def attack(self):
+        pass
+    def feed(self, feed_interval=10, hp_increase=10):  # Уменьшенный интервал кормления
+        return super().feed(feed_interval=feed_interval, hp_increase=hp_increase)
 
 class Fighter(Pokemon):
     def attack(self, enemy):
@@ -74,6 +93,9 @@ class Fighter(Pokemon):
         self.power += super_power
         result = super().attack(enemy)
         return result + f"\nБоец применил супер-атаку силой:{super_power} "
+    
+    def feed(self, feed_interval=20, hp_increase=15):  # Увеличенное увеличение HP
+        return super().feed(feed_interval=feed_interval, hp_increase=hp_increase)
         
 
 
